1b2d3e7e83f743cfc97486c32d9a5a48a957bb77
bobbyfavino311-oss <bobbyfavino311@gmail.com>
2026-07-21 22:34:28 -0700

    Remove KeyboardProvider native module from startup path

--- artifacts/neon-river/app/_layout.tsx
+++ artifacts/neon-river/app/_layout.tsx
@@ -17,7 +17,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect, useRef, useState } from 'react';
 import { Platform } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { KeyboardProvider } from 'react-native-keyboard-controller';
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
@@ -373,9 +373,7 @@ export default function RootLayout() {
                       <MultiplayerProvider>
                       <NotificationBridge>
                         <GestureHandlerRootView style={{ flex: 1 }}>
-                          <KeyboardProvider>
-                            <RootLayoutNav />
-                          </KeyboardProvider>
+                          <RootLayoutNav />
                         </GestureHandlerRootView>
                       </NotificationBridge>
                       </MultiplayerProvider>
